a18938071626749eb0c7a218f6763de68a137638
Laura Hughes <flaneuse@users.noreply.github.com>
2017-07-25 14:20:43 -0400

    process base

---
+++ R/00_setup.R
@@ -0,0 +1,38 @@
+
+# Setup file to establish libraries, constants, directories, etc. ---------
+# Bangladesh analysis 2.0
+# USAID | GeoCenter
+# Laura Hughes, lhughes@usaid.gov, 25 July 2017
+
+# working directories -----------------------------------------------------
+# raw data directory
+import_dir = '~/Documents/Bangladesh/rawdata/'
+pbs_dir = paste0(import_dir, 'BGD_2015_PBS/')
+gh_dir = '~/Documents/GitHub/bangladesh2017/'
+export_dir = paste0(gh_dir, 'exportedfromR/')
+
+# libraries ---------------------------------------------------------------
+library(dplyr) # version 0.7.1
+library(data.table) # version 1.10.4
+library(haven) # version 1.0.0
+library(tidyr) # version 0.6.3
+library(forcats) # version 0.2.0
+library(ggplot2) # version 2.2.1.9000
+library(sf) # version 0.5-1
+library(stringr) # version 1.2.0
+
+
+# generic function to convert labeled object to factor --------------------
+lab2factor = function(data, colname, ordered = FALSE) {
+  dict = data.frame(level = attr(data[[colname]], 'labels')) 
+  dict  = dict %>% 
+    mutate(label = row.names(dict))
+  
+  data[[colname]] =  factor(data[[colname]],
+                           levels = dict$level, 
+                           labels = dict$label,
+                           ordered = ordered)
+  
+  return(data)
+}
+
---
+++ R/01_PBS2015_modA_hhbase_clean.R
@@ -0,0 +1,34 @@
+# 01_PBS2015_modA_hhbase_clean.R ------------------------------------------
+# Bangladesh analysis 2.0; clean household base characteristics
+# Code to clean up the household base module from the Bangladesh Integrated Household Survey, 2015
+# Data downloaded 14 June 2017 from Harvard's Dataverse: 
+# https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/BXSYEL
+# USAID | GeoCenter
+# Laura Hughes, lhughes@usaid.gov, 25 July 2017
+
+
+
+# import data -------------------------------------------------------------
+hh = read_dta(paste0(pbs_dir, '001_r2_mod_a_male.dta'))
+
+
+# Clean up vars -----------------------------------------------------------
+
+hh = hh %>% 
+  # remove hh respondent, hh father id
+  select(-a10, -a12) %>% 
+  # rename vars
+  rename(head_id = a11,
+         religion = a13,
+         language = a14,
+         ethnic = a15) %>% 
+  # factorize
+  lab2factor('hh_type') %>% 
+  lab2factor('religion') %>% 
+  lab2factor('language') %>% 
+  lab2factor('ethnic') %>% 
+  # create copy
+  mutate(hh_id = a01,
+         ftf_flag = ifelse(hh_type %like% 'Feed', 1, 0)) %>% 
+  # filter only those hh that responded to entire survey
+  filter(flag_a == 1)
